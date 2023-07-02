44ee3e888b415cc1f2fcb89e4b4852d10ba4c8ef
Spandan-Bhattacharya <95448248+Spandan-Bhattacharya@users.noreply.github.com>
2023-07-03 05:09:52 +0530

    Improved responsiveness
    Canvas size is now variable and the snake is much more space efficient

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var growth;
 var count;
 var alive=1;
+var eaat=1;
+var hei=visualViewport.height;
+var wid=visualViewport.width;
+// var cwid=(wid*.8)
+// var chei=hei/2
+var cwid=(wid/2)-((wid/2)%10);
+var chei=hei/2 -((hei/2)%10);
 var snake = {
   xHead: 10,
   yHead: 10,
@@ -12,11 +19,12 @@ var snake = {
 };
 
 var food = {
-  x: randomGenerator(0, 39) * 10 + 5,
-  y: randomGenerator(0, 39) * 10 + 5,
+  //here
+  x: randomGenerator(0, (cwid/10)-1) * 10 + 5,
+  y: randomGenerator(0, (chei/10)-1) * 10 + 5,
 };
 function score() {
-  document.getElementById("score").innerHTML = "SCORE:" + snake.length;
+  document.getElementById("score").innerHTML = "SCORE:" + snake.length*10;
 }
 function mover() {
   snake.xHead += snake.xSpeed;
@@ -24,36 +32,45 @@ function mover() {
 }
 
 var canvas = document.getElementById("myCanvas");
+canvas.height=chei;
+canvas.width=cwid;
 var ctx = canvas.getContext("2d");
 
 //  ctx.translate(10, 10);
 // die();
+
 function movement() { 
+ 
   death();
   eat();
   score();
-  if (growth == 0) {
+  if (growth == 0&&alive) {
     for (i = 0; i < snake.length - 1; i++) {
       snake.bodyX[i] = snake.bodyX[i + 1];
       snake.bodyY[i] = snake.bodyY[i + 1];
     }
     snake.bodyX[snake.length - 1] = snake.xHead;
     snake.bodyY[snake.length - 1] = snake.yHead;
-  } else {
+  } else if(alive) {
     snake.bodyX[snake.length - 1] = snake.xHead;
     snake.bodyY[snake.length - 1] = snake.yHead;
   }
-  mover();
+
+    mover();
+  
+
+  
+ 
 }
 function drawer() {
   var i;
   ctx.beginPath();
-  ctx.clearRect(-10, -10, 400, 400);
+  ctx.clearRect(-10, -10, cwid, chei);
   ctx.closePath();
 
   ctx.beginPath();
   ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, 400, 400);
+  ctx.fillRect(0, 0, cwid, chei);
   ctx.closePath();
 
   ctx.beginPath();
@@ -61,8 +78,10 @@ function drawer() {
   ctx.fillStyle = "Red";
   ctx.fill();
   ctx.closePath();
-  for (i = 0; i < snake.length; i += 10) {
+  for (i = 0; i < snake.length; i++) {
+    ctx.lineWidth=2;
     ctx.beginPath();
+   
     ctx.strokeStyle = "#FFFFFF";
     ctx.strokeRect(snake.bodyX[i], snake.bodyY[i], 10, 10);
     ctx.stroke();
@@ -70,7 +89,8 @@ function drawer() {
     ctx.closePath();
   }
   ctx.beginPath();
-  ctx.strokeStyle = "white";
+  ctx.lineWidth=2;
+  ctx.strokeStyle = "red";
   ctx.rect(snake.xHead, snake.yHead, 10, 10);
   ctx.stroke();
   ctx.closePath();
@@ -85,8 +105,8 @@ function drawer() {
   }
 }
 
-setInterval(drawer, 10);
- setInterval(movement, 10);
+setInterval(drawer, 100);
+ setInterval(movement, 100);
 function eat() {
   if (
     Math.sqrt(
@@ -94,36 +114,38 @@ function eat() {
         Math.pow(snake.yHead + 5 - food.y, 2)
     ) < 10
   ) {
-    snake.length += 10;
+    snake.length +=1;
     growth = 1;
-    food.x = randomGenerator(0, 39) * 10 + 5;
-    food.y = randomGenerator(0, 39) * 10 + 5;
+    //here
+   
+    food.x = randomGenerator(0, (cwid/10)-1) * 10 + 5;
+    food.y = randomGenerator(0, (chei/10)-1) * 10 + 5;
   } else {
     growth = 0;
   }
 }
 function right() {
   if (snake.xSpeed == 0) {
-    snake.xSpeed = 1;
+    snake.xSpeed = 10;
     snake.ySpeed = 0;
   }
 }
 function left() {
   if (snake.xSpeed == 0) {
-    snake.xSpeed = -1;
+    snake.xSpeed = -10;
     snake.ySpeed = 0;
   }
 }
 function up() {
   if (snake.ySpeed == 0) {
     snake.xSpeed = 0;
-    snake.ySpeed = -1;
+    snake.ySpeed = -10;
   }
 }
 function down() {
   if (snake.ySpeed == 0) {
     snake.xSpeed = 0;
-    snake.ySpeed = 1;
+    snake.ySpeed = 10;
   }
 }
 function randomGenerator(upper, lower) {
@@ -132,10 +154,11 @@ function randomGenerator(upper, lower) {
 
 function death() {
   if (
-    snake.xHead > 390 ||
+    //here
+    snake.xHead > cwid-10 ||
     snake.xHead < 0 ||
     snake.yHead < 0 ||
-    snake.yHead > 390
+    snake.yHead > chei-10
   ) {
     die();
   }
@@ -153,6 +176,7 @@ function die() {
   snake.ySpeed = 0;
  
 }
+
 function reset() {
   snake.xSpeed = 0;
   snake.ySpeed = 0;
